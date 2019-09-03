6b32713de25fd6e8a9cb4c1eb964173a512231ac
Deepak Singh <deepaksingh@saffrontech.net>
2019-09-03 17:34:52 +0530

    title

--- app/code/Saffron/Bannerslider/Block/Cbdxcms.php
+++ app/code/Saffron/Bannerslider/Block/Cbdxcms.php
@@ -1,20 +1,13 @@
 <?php
 /**
 * Copyright © 2015 Saffron.com. All rights reserved.
-
 * @author Saffron Team <contact@Saffron.com>
 */
-
 namespace Saffron\Bannerslider\Block;
-
 class Cbdxcms extends \Magento\Framework\View\Element\Template {
-
   public function __construct(\Magento\Catalog\Block\Product\Context $context, array $data = []) {
-
         parent::__construct($context, $data);
-
     }
-
 	
 	protected function _prepareLayout() {
 		
