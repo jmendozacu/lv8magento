f7e5b3dca42e8b0e5ee900c8fa9ac89adca25549
Day User <deepaksingh@saffrontech.net>
2020-01-15 19:15:25 +0530

    form

--- app/code/Saffron/News/Block/Index/Recentvideo.php
+++ app/code/Saffron/News/Block/Index/Recentvideo.php
@@ -39,7 +39,7 @@ class Recentvideo extends \Magento\Framework\View\Element\Template {
 	}*/
 	
 	public function getNewsAllPost(){
-	  $page= $_GET['page'];
+	  $page= $_GET['row'];
      	
 	$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
    	$newcategories = $objectManager->create('Saffron\News\Model\ResourceModel\Newspost\Collection')
