511c7c323090f3d18703193c33413a29e6509c3f
Day User <deepaksingh@saffrontech.net>
2020-02-20 11:58:42 +0530

    new

---
+++ app/code/SS/NewsletterAjax/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Spencer Steiner.
+ * See LICENSE.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'SS_NewsletterAjax', __DIR__);
